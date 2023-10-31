d4fe0b1c78ae45490de965392063d65305d7423e
lucas <lucasmacuna@gmail.com>
2023-10-31 03:25:53 -0300

    Logica compra

--- JS/compraVirtual.js
+++ JS/compraVirtual.js
@@ -31,7 +31,7 @@ const vistaArticulos=()=>{
            <p><b>Ahora: $${art.precio}</b></p>
            <p><b>${art.stock}</b></p>
             </div>
-            <button class="btn-agregar">Agregar</button>
+            <button onclick="modalVenta(id)" id=${art.codigo} class="btn-agregar">Agregar</button>
         </div>
         `
     });
@@ -39,3 +39,40 @@ const vistaArticulos=()=>{
 
 
 vistaArticulos()
+
+
+
+
+const modalVenta=(id)=>{
+   console.log(id)
+   let articulos=localStorage.getItem("articulos")
+         articulos=JSON.parse(articulos)
+
+          Swal.fire({
+           
+            html:`
+            
+               <div style="text-align-center !important;width:200px;background:white;color:black">
+               <h4>Agregar cantidad</h4>
+               <input id="cantidad" placeholder="cantidad"></input><br>
+               <button id=${id} onclick="confirmCantidad(id)">Añadir cantidad</button>
+               </div>
+            `,
+            showConfirmButton:false,
+            showCancelButton:true,
+            cancelButtonText:'<span id="salir" style="border:none !important;font-size:16px;color:white;font-weight:bold" value="false">X</span>',
+          })
+
+}
+
+
+
+
+const confirmCantidad=(id)=>{
+   let cantidad=document.querySelector("#cantidad").value
+        console.log(id)
+        console.log(`la cantidad es ${cantidad}`)
+        
+
+        
+}
